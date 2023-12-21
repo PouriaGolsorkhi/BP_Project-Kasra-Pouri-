df07641ed11b32b31d7bdff9e0176f6c17f398e6
kasrafouladi <152831297+kasrafouladi@users.noreply.github.com>
2023-12-21 16:25:12 +0330

    Update main.cpp
    طرح اولیه

--- main.cpp
+++ main.cpp
@@ -1,1 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+void mini_search_en(){
+	/*
+	It will searches parts of program
+	e.g:
+		search(sign up) -> sign up
+	func
+	-------------------------
+	map
+	-------------------------
+	user
+	-------------------------
+	*/
+	// show res
+}
+
+void sign_up(){
+		
+}
+
+void sign_in(){
+	
+}
+
+void welcome(){
+    sign_up();
+    sign_in();
+}
+
+void user_history(/*user id*/){
+	
+}
+
+void scoreboard(){
+	user_history(/*user id*/);
+}
+
+void solve(){
+	
+}
+
+void play(){
+	old_map();
+	new_map();
+}
+
+void dashboard(/*user id*/){
+	scoreboard();
+	user_history(/*user id*/);
+	play();
+}
+
+void check(string &s){
+	if(s == "search")
+		mini_search_en();
+	
+}
+
+int main(){
+    opening();
+    welcome();
+    dashboard();
+}
