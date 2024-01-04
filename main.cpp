10bdf1869a808ae848dd8fc77ec9616e2015432a
kasrafouladi <152831297+kasrafouladi@users.noreply.github.com>
2024-01-04 14:24:15 +0330

    .

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include<bits/stdc++.h>
+#include<conio.h>
+#include<iomanip>
+#include<stdlib.h>
 using namespace std;
 
 using ll = long long;
 
 void sing_up(vector<pair<string, string>> &users);
 void sing_in(vector<pair<string, string>> &users);
+
+void head(){
+	cout << "MAZE-MAVERTIK\n";
+	cout << "Created by Kasra Fouladi & Pouria Golsorkhi\n";
+	cout << "-------------------------------------------------------------\n\n";
+	return;
+}
 
 int get_ind(vector<pair<string, string>> &users, string &s){
 	for(int i = 0; i < users.size(); ++i)
@@ -13,11 +23,38 @@ int get_ind(vector<pair<string, string>> &users, string &s){
 	return -1;
 }
 
+void psw(string &s){
+	s = "";
+	cout << "\b";
+	while(true){
+		char c = getch();
+		if(c == 13)
+			return;
+		else if(c == '\b'){
+			if(s.size()){
+				s.pop_back();
+				cout << "\b\b";
+			}
+		}
+		else{
+			cout << c;
+			sleep(0.02);
+			cout << "\b ";
+			if(s.size())
+				cout << '\b';
+			cout << "*";
+			s.push_back(c);
+		}
+	}
+	return;
+}
+
 void sing_up(vector<pair<string, string>> &users){
 	string s1, s2;
 	for(bool b = false; true; b = true){
 		system("cls");
-		cout << "if you have an account and want to sing in write \"sing in\" and press enter" << '\n';
+		head();
+		cout << "If you have an account and want to sing in write \"sing in\" and press enter" << '\n';
 		cout << "-------------------\n";
 		if(b)
 			cout << "username is taken" << '\n';
@@ -32,23 +69,25 @@ void sing_up(vector<pair<string, string>> &users){
 			break;
 	}
 	system("cls");
-	cout << "if you have an account and want to sing in write \"sing in\" and press enter" << '\n';
+	head();
+	cout << "If you have an account and want to sing in write \"sing in\" and press enter" << '\n';
 	cout << "-------------------\n";
 	cout << "username: " << s1 << '\n';
 	cout << "password: ";
-	getline(cin, s2);
+	//getline(cin, s2);
+	psw(s2);
 	if(s2 == "sing in"){
 		sing_in(users);
 		return;	
 	}
 	ofstream us("./accounts/users.txt");
 	ofstream ps("./accounts/pass.txt");
-	if(!us.is_open() || !ps.is_open()){
-		cerr << "Can't find data" << '\n';
-		exit(2);
+	for(auto &e: users){
+		us << e.first << '\n';
+		ps << e.second << '\n';
 	}
-	us << s1;
-	ps << s2;
+	us << s1 << '\n';
+	ps << s2 << '\n';
 	us.close();
 	ps.close();
 	return;
@@ -58,18 +97,22 @@ void sing_in(vector<pair<string, string>> &users){
 	string s1, s2;
 	for(bool b = false; true; b = true){
 		system("cls");
-		cout << "if you have an account and want to sing in write \"sing up\" and press enter" << '\n';
+		head();
+		cout << "If want to create account write \"sing up\" and press enter" << '\n';
 		cout << "-------------------\n";
 		if(b)
 			cout << "username or password is in correct" << '\n';
 		cout << "username: ";
 		getline(cin, s1);
-		if(s1 == "sing up"){
-			sing_up(users);
-			return;
+		if(s1 == "sing in"){
+			sing_in(users);
+			return;	
 		}
 		cout << "password: ";
-		getline(cin, s2);
+		/////////////////
+		//getline(cin, s2);
+		psw(s2);
+		/////////////////
 		if(s2 == "sing in"){
 			sing_in(users);
 			return;	
@@ -100,6 +143,7 @@ void enter(){
 	ps.close();
 	for(bool b = false; true; b = true){
 		system("cls");
+		head();
 		cout << "Have an account? (y/n)" << '\n';
 		if(b)
 			cout << "invalid input, try again" << '\n';
@@ -115,25 +159,31 @@ void enter(){
 }
 
 void menu(){
-	system("cls");
-	cout << "Menu:" << '\n';
-	cout << "  1. Create a New Map" << '\n';
-	cout << "    - 1.1 Easy" << '\n';
-	cout << "    - 1.2 Hard" << '\n';
-	cout << "  2. Playground" << '\n';
-	cout << "    - 2.1 Choose from Existing Maps" << '\n';
-	cout << "    - 2.2 Import a Custom Map" << '\n';
-	cout << "  3. Solve a Maze" << '\n';
-	cout << "    - 3.1 Choose from Existing Maps" << '\n';
-	cout << "    - 3.2 Import a Custom Map" << '\n';
-	cout << "  4. History" << '\n';
-	cout << "  5. Leaderboard" << '\n';
-	cout << "  6. Exit" << '\n';
-	
+	//system("Color 0D");
+	//for(bool b = false; true; b = true){
+		system("cls");
+		head();
+		cout << "Menu:" << '\n';
+		cout << "  1. Create a New Map" << '\n';
+		cout << "    - 1.1 Easy" << '\n';
+		cout << "    - 1.2 Hard" << '\n';
+		cout << "  2. Playground" << '\n';
+		cout << "    - 2.1 Choose from Existing Maps" << '\n';
+		cout << "    - 2.2 Import a Custom Map" << '\n';
+		cout << "  3. Solve a Maze" << '\n';
+		cout << "    - 3.1 Choose from Existing Maps" << '\n';
+		cout << "    - 3.2 Import a Custom Map" << '\n';
+		cout << "  4. History" << '\n';
+		cout << "  5. Leaderboard" << '\n';
+		cout << "  6. Exit" << '\n';
+	//}
+	return;
 }
-
+// - : undo
 int main(){
+	system("Color 0A");
 	enter();
 	menu();
+	//forget();
 	return 0;
 }
